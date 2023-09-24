16346567fc42e95d42ad52391848e81b7466f8a9
Dawson Claybrook <joystickclay@gmail.com>
2023-09-24 15:19:58 -0600

    Changed config for railway connection to SQL

--- config/connection.js
+++ config/connection.js
@@ -7,14 +7,14 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 }
 else {
-    sequelize = new Sequelize(
+    const sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PASSWORD,
         {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
+          host: process.env.DB_HOST,
+          dialect: 'mysql',
+          port: process.env.DB_PORT,
         }
     );
 }
